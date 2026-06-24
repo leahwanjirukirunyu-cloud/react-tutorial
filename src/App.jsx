8a558d96ca42b6438a7bd52fd5fb315b47c0e3b7
leahwanjirukirunyu-cloud <leahwanjirukirunyu@gmail.com>
2026-06-24 12:58:49 +0300

    Add the files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+        </div>
+        <div>
+          <h1>leahkirunyu</h1>
+          
+        </div>
+ 
+      </section>
+
+
+    </>
+  )
+}
+
+export default App
