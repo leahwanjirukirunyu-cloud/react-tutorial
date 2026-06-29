f9c7278906aa997b7adf886a584c6e54208ca55c
leahwanjirukirunyu-cloud <leahwanjirukirunyu@gmail.com>
2026-06-29 08:42:09 +0300

    create header component

---
+++ src/Header.jsx
@@ -0,0 +1,24 @@
+
+//Header component
+function Header(){
+
+    return(
+    <header>
+        <h1>MY PORTFOLIO</h1>
+        <nav>
+            <ol>
+                NAME:Leah Wanjiru Kirunyu<br></br>
+               EMAIL:leahwanjirukirunyu@gmail.com
+            </ol>
+            <h2>ABOUT ME :</h2>
+            <p>I am student at Zetech University persuing,
+                 software engineering class of 2026.</p>
+             <p> I am passionate about it cause it requires contionous learning.</p>
+
+
+        </nav>
+    </header>
+
+    );
+}
+export default Header                          
