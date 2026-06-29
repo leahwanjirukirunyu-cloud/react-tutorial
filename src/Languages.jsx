8b5ddc64afeba1015544bd8c47d77ba44716a72d
leahwanjirukirunyu-cloud <leahwanjirukirunyu@gmail.com>
2026-06-29 08:34:00 +0300

    create languages component

---
+++ src/Languages.jsx
@@ -0,0 +1,17 @@
+function Languages(){
+
+    
+return(
+    <ol>
+        <li>PHP-for backend</li>
+        <li>HTML-for frontend</li>
+        <li>CSS-for styling</li>
+        <li>MYSQL-for database</li>
+        <li>TAILWIND-CSS - css framework</li>
+        <li>LARAVEL-php framework</li>
+        <li>REACT -javascript library</li>
+    </ol>
+
+);
+}
+export default Languages
